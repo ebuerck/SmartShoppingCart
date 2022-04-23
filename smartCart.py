76be10bfc2836381dc6d24ecdf6dc284086e24ab
Nick Sutherland <78757306+nrsutherland00@users.noreply.github.com>
2022-04-23 12:10:43 -0500

    Update smartCart.py
    Almost finished. Just have to integrate the stop barcode and finish the excel file

--- smartCart.py
+++ smartCart.py
@@ -7,6 +7,9 @@ df = pd.read_excel('ItemData.xlsx')
 priceList = df['Price'].tolist()
 barcodeList = df['Barcode #'].tolist()
 descriptionList = df['Description'].tolist()
+File_object = open("Receipt.txt", "w")
+File_object.write("Smart Shopping Cart\n")
+File_object.write("--------------------\n")
 # print(df)
 # print(df.columns.tolist())
 # print(barcodeList)
@@ -20,6 +23,10 @@ while scode != 55555:
     i = 0
     for x in barcodeList:
         if x == scode:
+            File_object.write(str(descriptionList[i]) + "          " + str(priceList[i]) + "\n")
             totalPrice += priceList[i]
         i += 1
-print(totalPrice)
+print("Thank you for shopping!\n")
+File_object.write("--------------------\n")
+File_object.write("Total Price:          " + str(totalPrice) + "\n")
+File_object.write("Thank you for shopping!")
